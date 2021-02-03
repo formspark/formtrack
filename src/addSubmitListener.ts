7e43e4fb11b1bf8a31d875428a3a5586d5d24912
Bjorn Krols <krols.bj@gmail.com>
2021-02-03 09:18:14 +0100

    Adds dataset check

--- src/addSubmitListener.ts
+++ src/addSubmitListener.ts
@@ -9,32 +9,34 @@ const addSubmitListener = () => {
           const formEventTarget = event.target;
           if (formEventTarget) {
             const formElement = formEventTarget as HTMLFormElement;
-            const shouldTrack =
-              typeof formElement.dataset.formtrack !== "undefined";
-            if (shouldTrack) {
-              const searchParameters = new URLSearchParams(
-                window.location.search
-              );
-              const supportedParameterNames = [
-                "referrer",
-                "utm_campaign",
-                "utm_content",
-                "utm_medium",
-                "utm_source",
-                "utm_term",
-              ];
-              for (let i = 0; i < supportedParameterNames.length; i++) {
-                const parameterName = supportedParameterNames[i];
-                const id = `formtrack_${parameterName}`;
-                const name = parameterName;
-                const value = searchParameters.get(parameterName);
-                if (value) {
-                  appendOrUpdateInput({
-                    formElement,
-                    id,
-                    name,
-                    value,
-                  });
+            if (formElement.dataset) {
+              const shouldTrack =
+                typeof formElement.dataset.formtrack !== "undefined";
+              if (shouldTrack) {
+                const searchParameters = new URLSearchParams(
+                  window.location.search
+                );
+                const supportedParameterNames = [
+                  "referrer",
+                  "utm_campaign",
+                  "utm_content",
+                  "utm_medium",
+                  "utm_source",
+                  "utm_term",
+                ];
+                for (let i = 0; i < supportedParameterNames.length; i++) {
+                  const parameterName = supportedParameterNames[i];
+                  const id = `formtrack_${parameterName}`;
+                  const name = parameterName;
+                  const value = searchParameters.get(parameterName);
+                  if (value) {
+                    appendOrUpdateInput({
+                      formElement,
+                      id,
+                      name,
+                      value,
+                    });
+                  }
                 }
               }
             }
